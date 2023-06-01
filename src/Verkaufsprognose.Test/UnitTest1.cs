cffc3200159015f5ad45b9a8476947c670108260
Max Brauer <max.brauer@byte-physics.de>
2023-06-01 20:23:59 +0200

    add project files

---
+++ src/Verkaufsprognose.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Verkaufsprognose.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
